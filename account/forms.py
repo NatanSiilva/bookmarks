7c4e8cf9c16b6c053721b34652653d1ab611d849
NatanSiilva <contato@natandev.com.br>
2022-01-18 20:38:42 -0300

    feat: auth implemented

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,37 @@
+from django.contrib.auth.models import User
 from django import forms
+from.models import Profile
 
 
 class LoginForm(forms.Form):
-      username = forms.CharField(label='Username', max_length=100)
-      password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password Confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords do not match')
+        return password2
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
