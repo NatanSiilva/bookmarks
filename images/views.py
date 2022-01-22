c49f1ba48021d7df100987d75b3eba0706735e0f
NatanSiilva <contato@natandev.com.br>
2022-01-21 23:32:06 -0300

    feat: ranking with redis

--- images/views.py
+++ images/views.py
@@ -12,6 +12,14 @@ from .models import Image
 from common.decorators import ajax_required
 from actions.utils import create_action
 
+import redis
+from django.conf import settings
+
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
+
 
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
@@ -37,7 +45,9 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
 
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 # @ajax_required
@@ -76,3 +86,18 @@ def image_list(request):
     if is_ajax(request=request):
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
